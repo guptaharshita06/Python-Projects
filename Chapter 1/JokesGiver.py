9e269f86e8fcfabae5ddb5402ca56c9afeca6633
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-08-15 19:25:52 +0530

    Create JokesGiver.py

---
+++ Chapter 1/JokesGiver.py
@@ -0,0 +1,15 @@
+import pyjokes
+
+# print("Printing Jokes...")
+#This prints a random joke.
+joke = pyjokes.get_joke()
+print(joke)
+
+"""
+So thanks
+that is my program
+"""
+
+#There are two types of module which are 
+#1. Built-in modules
+#2. External modules
