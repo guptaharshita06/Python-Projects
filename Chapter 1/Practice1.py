45895ce35c07b316ad510769ebe6f2fbcd76b44b
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-08-15 19:26:37 +0530

    Create Practice1.py

---
+++ Chapter 1/Practice1.py
@@ -0,0 +1,31 @@
+#Write a program to print Twinkle twinkle little start poem in python.
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the traveler in the dark
+Thanks you for your tiny spark,
+How could he see where to go,
+If you did not twinkle so?
+
+In the dark blue sky you keep,
+Often through my curtains peep
+For you never shut your eye,
+Till the sun is in the sky.
+
+As your bright and tiny spark
+Lights the traveler in the dark,
+Though I know not what you are,
+Twinkle, twinkle, little star.''')
+
+#Use REPL and print the table of 5 using it.(Read Eval Print Loop)
+#Install an external module and use it to perform an operation of your intrest.
+#Write a python program to print the contents of a directory using the os module. Search online for the function which does that.
+#Label the program written in problem 4 with comments.
