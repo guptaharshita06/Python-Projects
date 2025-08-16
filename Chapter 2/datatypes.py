e1ee8da0695fb4609530ea00000671550190cbe9
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-08-16 13:00:18 +0530

    Create datatypes.py

---
+++ Chapter 2/datatypes.py
@@ -0,0 +1,21 @@
+a = 1 # a is a integer
+
+b = 5.22 # b is a floating point number
+
+c = "Harshita" # c is a string
+
+d = False # d is a boolean variable
+
+e = None # e is a none type variable
+
+
+#typecasting
+
+a = 31
+t = type(a) # class <int>
+print(t)
+
+b = "31.2"
+c = float(b)
+d = type(c)
+print(d)
