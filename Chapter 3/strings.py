7d9f5ad291eae944bd3958b17ef686b3ce7c073f
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-08-17 21:35:56 +0530

    Create strings.py

---
+++ Chapter 3/strings.py
@@ -0,0 +1,11 @@
+a = "Harry" #double quoted string
+b = 'Harry' #single quoted string
+c = '''Harry''' #triple quoted string
+
+#strings are immutable
+
+name = "Harshita"
+nameshort = name[0:2]
+print(nameshort) #prints "Ha" from the string "Harshita" from index 0 to 2
+character1 = name[1]
+print(character1) #prints "a" from the string "Harshita" from index
