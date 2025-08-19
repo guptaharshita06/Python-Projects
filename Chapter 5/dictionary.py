9a5b428852b8e0667c1b44f39f7a0da23255a34b
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-08-19 21:11:04 +0530

    Create dictionary.py

---
+++ Chapter 5/dictionary.py
@@ -0,0 +1,17 @@
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23
+}
+
+#List me list
+marks1 = [["Harry", 100], ["Shubham", 56], ["Rohan", 23]]
+
+print(marks, type(marks))
+print(marks["Harry"])
+
+# Properties of python dictionary
+# 1. It is unordered
+# 2. It is mutable
+# 3. It is indexed
+# 4. Cannot contain duplicate keys.
