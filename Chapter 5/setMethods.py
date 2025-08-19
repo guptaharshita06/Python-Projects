f4f93b5590561927915a2b20430e98a0915784cd
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-08-19 21:12:30 +0530

    Create setMethods.py

---
+++ Chapter 5/setMethods.py
@@ -0,0 +1,16 @@
+s = {1,5,32,54,5,5,5, "Harry"}
+
+print(s, type(s))
+s.add(566)
+s.remove(1)
+s.pop()
+s.clear()
+# s.union()
+# s.intersection()
+print(s)
+print(len(s))
+
+#1. Sets are unordered
+#2. Sets are unindexed
+#3. There is no way to change items in sets
+#4. Sets contain duplicate values.
