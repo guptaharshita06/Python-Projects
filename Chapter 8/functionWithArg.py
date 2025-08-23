f56371160ece8f8b2ed8ddc44fa2e862943e38e9
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-08-23 20:26:27 +0530

    Create functionWithArg.py

---
+++ Chapter 8/functionWithArg.py
@@ -0,0 +1,10 @@
+def goodDay(name, ending):
+    print("Good day, " + name)
+    print(ending)
+
+# goodDay("Harry", "Thank You")
+# goodDay("Rohan", "Thank you")
+# goodDay("Divya", "Thank you")
+
+a = goodDay("Harry", "Thank you")
+print(a)
