2d418b751ea1566104c3501bd6059b9ae4085073
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-08-23 20:25:57 +0530

    Create function.py

---
+++ Chapter 8/function.py
@@ -0,0 +1,10 @@
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: "))
+    c = int(input("Enter your number: "))
+
+    average = (a+b+c)/3
+    print(average)
+
+avg()
+print("Thank you")
