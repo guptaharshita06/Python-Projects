6ba3770e023170e4d94930008817367fdc21ce81
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-08-24 20:13:44 +0530

    Create file.py

---
+++ Chapter 9/file.py
@@ -0,0 +1,13 @@
+'''
+a = "a very long string with emails"
+emails = []
+3 seconds
+'''
+
+f = open("file.txt")
+data = f.read()
+print(data)
+f.close()
+#Types of files
+#1. Text file
+#2. Binary file
