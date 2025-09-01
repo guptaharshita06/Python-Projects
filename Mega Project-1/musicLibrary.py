e846f520bef9224f80b1c11e1b20e88d3e777de2
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-09-01 23:52:55 +0530

    Create musicLibrary.py

---
+++ Mega Project-1/musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE&list=RDU47Tr9BB_wE&start_radio=1",
+    "march": "https://www.youtube.com/watch?v=5FpgE1mbmc0&list=RD5FpgE1mbmc0&start_radio=1",
+    "heavenly": "https://www.youtube.com/watch?v=spdeeDo0qyM&list=RDspdeeDo0qyM&start_radio=1",
+    "beauty": "https://www.youtube.com/watch?v=b7caaXPJ5zU&list=RDb7caaXPJ5zU&start_radio=1"
+}
